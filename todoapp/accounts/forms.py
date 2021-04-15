804e67b57882058cf14d5830d71bbdfeafcb0a38
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-04-15 12:27:55 +0200

    accounts app (models,views,forms,urls)

---
+++ todoapp/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import get_user_model
+
+class SignUpForm(UserCreationForm):
+    fields = ['username','email','password1','password2']
+    model = get_user_model()
+    
+    def __init__(self,*args,**kwargs):
+        super(SignUpForm, self).__init__(*args,**kwargs)
+        self.fields['username'].label = 'Pseudo'
+        self.fields['email'].label = 'Email'
+
--- todoapp/accounts/views.py
+++ todoapp/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.urls import reverse,reverse_lazy
+from accounts.forms import SignUpForm
+from django.views.generic.edit import CreateView
 
 # Create your views here.
+
+class SignUpView(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+
