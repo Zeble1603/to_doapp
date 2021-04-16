81d21181015a6287e2399f43b5f5818a0c904cb9
Zeblé <66326476+Zeble1603@users.noreply.github.com>
2021-04-16 10:13:03 +0200

    create/list views

--- todoapp/basic_app/views.py
+++ todoapp/basic_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-# Create your views here.
+from django.utils import timezone
+from django.views.generic import TemplateView, CreateView, DeleteView,ListView
+from .models import Task
+from .forms import TaskForm
+# Create your views
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "home.html"
+    
+    def get_queryset(self):
+        return Task.objects.filter(published_date__lte=timezone.now()).order_by("-due_date")
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = "task_form.html"
+    form_class = TaskForm
+    redirect_field_name = 'basic_app/home.html'
+    
+
+
